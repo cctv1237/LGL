bc61cbf05d29c0c4df3220b9b886bb39524faa4d
cctv1237 <marklf@163.com>
2015-06-25 15:49:08 +0800

    Initial Commit

---
+++ LogicGridLayout/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LogicGridLayout
+//
+//  Created by LF on 15/6/25.
+//  Copyright (c) 2015年 Beauty Sight Network Technology Co.,Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
