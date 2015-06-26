db56c293efd02557fe0b1d83e5795689867847d0
cctv1237 <marklf@163.com>
2015-06-26 17:07:38 +0800

    demo

--- LogicGridLayout/main.m
+++ LogicGridLayout/main.m
@@ -7,11 +7,39 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BSGridCalculator.h"
+#import "BSGridBlock.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+//        NSLog(@"Hello, World!");
+        
+        BSGridCalculator *gridCalculator = [[BSGridCalculator alloc] init];
+        
+        NSMutableArray *gridBlocks = [[NSMutableArray alloc] init];
+        
+//        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:5 ColSpan:2 Content:@"1"]];
+//        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:3 ColSpan:2 Content:@"2"]];
+//        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:2 ColSpan:2 Content:@"3"]];
+//        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:3 ColSpan:3 Content:@"4"]];
+//        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:3 ColSpan:1 Content:@"5"]];
+//        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:2 ColSpan:3 Content:@"6"]];
+        
+        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:4 ColSpan:2 Content:@"1"]];
+        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:2 ColSpan:2 Content:@"2"]];
+        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:6 ColSpan:2 Content:@"3"]];
+        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:6 ColSpan:4 Content:@"4"]];
+        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:2 ColSpan:5 Content:@"5"]];
+        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:7 ColSpan:3 Content:@"6"]];
+        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:5 ColSpan:4 Content:@"7"]];
+        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:8 ColSpan:3 Content:@"8"]];
+        [gridBlocks addObject:[[BSGridBlock alloc] initWithRowSpan:2 ColSpan:2 Content:@"9"]];
+
+        
+        gridCalculator.gridBlocks = gridBlocks;
+        [gridCalculator doGridCalculate];
+        
     }
     return 0;
 }
